619c08e2101754f388acc546f2df1493fe8014b9
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2022-02-12 20:54:56 -0300

    basic config

---
+++ backend/src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {console.log("Escutando na porta 3000")});
